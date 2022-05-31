a37680b45b63d11f30b38649aa93fb18d625cbd6
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2022-05-31 00:09:57 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,125 @@
+// <!-- /////// * CHAPTER 43 TO 48 * /////// -->
+
+// !-- // * Question No 01 Start * // --
+
+// function alex(){
+//     alert("Hello Sir")
+// }
+
+// <!-- // * Question No 01 Completed * // -->
+
+// !-- // * Question No 02 Start * // --
+
+// function cov1(){
+//     alert("IPhone 6s plus")
+// }
+// function cov2(){
+//     alert("Samsung Galaxy S6 Edge 64GB Gold")
+// }
+// function cov3(){
+//     alert("Lg v10 GIFs - Get the best gif on GIFER")
+// }
+// function cov4(){
+//     alert("Huawei P8 Lite Used")
+// }
+
+// <!-- // * Question No 02 Completed * // -->
+
+// !-- // * Question No 03 Start * // --
+
+//   function del(){
+//       document.getElementById("name1").innerHTML=""
+//   }
+//   function del1(){
+//       document.getElementById("name2").innerHTML=""
+//   }
+//   function del2(){
+//       document.getElementById("name3").innerHTML=""
+//   }
+//   function del3(){
+//       document.getElementById("name4").innerHTML=""
+//   }
+
+// <!-- // * Question No 03 Completed * // -->
+
+// !-- // * Question No 04 Start * // --
+
+    // function on(){
+    //     document.getElementById('bulb').src="./on.jpg"
+    // }
+    // function off(){
+    //     document.getElementById("bulb").src="./photo-1532007271951-c487760934ae.jpg"
+    // }
+
+// <!-- // * Question No 04 Completed * // -->
+
+// !-- // * Question No 05 Start * // --
+
+//   var clicks = 0;
+//   function incr(){
+//       clicks += 1;
+//       var a = document.getElementById("incre"),innerHTML = clicks;
+//   }
+//   var clicker = ;
+//   function decre(){
+//       clicker -= ;
+//       document.getElementById("incre").innerHTML = clicker;  
+//   }
+
+// <!-- // * Question No 05 Completed * // -->
+
+
+// <!-- /////// * CHAPTER 49 TO 52 START * /////// -->
+
+// !-- // * Question No 01 & 02 Start * // --
+
+    // function reals(){
+    //     var hmm = document.getElementById("realdoc")
+    //     hmm.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce maximus accumsan auctor. Pellentesque egestas interdum ligula sit amet iaculis "
+    // }
+
+// <!-- // * Question No 01 & 02 Completed * // -->
+
+// !-- // * Question No 03 Start * // --
+
+    //  function del(){
+    //      document.getElementById("td1").innerHTML=""
+    //  }
+    //  function del1(){
+    //      document.getElementById("td2").innerHTML=""
+    //  }
+    //  function del2(){
+    //      document.getElementById("td3").innerHTML=""
+    //  }
+    //  function del3(){
+    //      document.getElementById("td4").innerHTML=""
+    //  }
+    //  function gam(){
+    //      var user =prompt("Enter Your Name")
+    //      var user1 =prompt("Enter Your Class")
+    //      var user2 =prompt("Enter Your Position")
+
+    //      document.getElementById("name").innerHTML=user
+    //      document.getElementById("coll").innerHTML=user1
+    //      document.getElementById("post").innerHTML=user2
+    //  }
+    //  function gam1(){
+    //      var user =prompt("Enter Your Name")
+    //      var user1 =prompt("Enter Your Class")
+    //      var user2 =prompt("Enter Your Position")
+
+    //      document.getElementById("name1").innerHTML=user
+    //      document.getElementById("coll2").innerHTML=user1
+    //      document.getElementById("post3").innerHTML=user2
+    //  }
+    //  function gam2(){
+    //      var user =prompt("Enter Your Name")
+    //      var user1 =prompt("Enter Your Class")
+    //      var user2 =prompt("Enter Your Position")
+
+    //      document.getElementById("name2").innerHTML=user
+    //      document.getElementById("coll5").innerHTML=user1
+    //      document.getElementById("post6").innerHTML=user2
+    //  }
+
+ // * Question No 03 Completed * // -->
